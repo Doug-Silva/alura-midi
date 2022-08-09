c5216f6d5c25643f9af9c7b477c4bf19aba2416d
Douglas Silva <silvatec.br@gmail.com>
2022-08-08 22:37:55 -0300

    funcoes anonimas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-alert('Hello World!');
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+//referencia que não vai se modificar, vai ter seu valor constante
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while (contador < 9) {
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom('#som_tecla_pom');
+    }
+    contador = contador + 1;
+}
