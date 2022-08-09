f685d02e68537751576363c5c8323f578cf3a91e
Douglas Silva <silvatec.br@gmail.com>
2022-08-09 01:22:13 -0300

    projeto finalizado 1.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,37 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
 //referencia que não vai se modificar, vai ter seu valor constante
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-//enquanto
-while (contador < 9) {
-    listaDeTeclas[contador].onclick = function () {
-        tocaSom('#som_tecla_pom');
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //templete string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
     }
-    contador = contador + 1;
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }
